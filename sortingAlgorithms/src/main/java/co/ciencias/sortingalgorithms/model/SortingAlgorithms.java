98bf58ba18ef1e005d7dfa535d5fd03588b173ce
JasFasola <buitr4go@gmail.com>
2026-04-16 18:53:54 -0500

    quickSort & selectionSort

--- sortingAlgorithms/src/main/java/co/ciencias/sortingalgorithms/model/SortingAlgorithms.java
+++ sortingAlgorithms/src/main/java/co/ciencias/sortingalgorithms/model/SortingAlgorithms.java
@@ -45,4 +45,44 @@ public class SortingAlgorithms {
         return arr;
     }
     
+    public int[] insertionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 1; i < n; i++){
+            int temp = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > temp){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+        return arr;
+    }
+    
+    public void quickSort(int[] arr, int start, int end){
+        if(end <= start) return;
+        int pivot = partition(arr, start, end);
+        quickSort(arr, start, pivot -1);
+        quickSort(arr, pivot+1, pivot -1);
+
+    }
+    
+    public int partition(int[] arr, int start, int end){
+        int pivot = arr[end];
+        int i = start - 1;
+        for(int j = start; j <= end -1; j++){
+            if(arr[j] < pivot){
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[end];
+        arr[end] = temp;
+        
+        return i;
+    }
 }
